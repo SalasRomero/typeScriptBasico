9e24ba5a62d4ad21cee3d2cc23b4ff08edbe9f52
David Salas <dasa.19983@gmail.com>
2024-01-06 23:55:15 -0500

    Priorizar composicion sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,16 +2,28 @@ export class Person{
     // public name:string;
     // private address:string;
 
-    constructor(public name:string, public address:string = 'No address'){}
+    constructor(public firstName:string,public lastName:string,public address:string = 'No address'){}
 
 }
 
-export class Hero extends Person{
-    constructor(public alterEgo:string, public age:number, public realName:string){
-        super(realName,'New York');
+// export class Hero extends Person{
+
+//     constructor(public alterEgo:string, public age:number, public realName:string){
+//         super(realName,'New York');
+//     }
+// }
+
+export class Hero {
+    // public person: Person;
+    
+    constructor(public alterEgo:string, public age:number, public realName:string,
+        public person:Person){
+        // this.person = new Person(realName);
     }
 }
 
-const iroman = new Hero('Iroman',45,'Tony');
+const person = new Person('Tony','stark','New York');
+
+const iroman = new Hero('Iroman',45,'Tony',person);
 
 console.log(iroman);
