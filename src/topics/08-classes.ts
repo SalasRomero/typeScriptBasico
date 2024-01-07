9c089d2f0848644c24873d2b29474a96b1e0f377
David Salas <dasa.19983@gmail.com>
2024-01-06 23:45:35 -0500

    Extender una clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,10 +2,16 @@ export class Person{
     // public name:string;
     // private address:string;
 
-    constructor(public name:string, public address:string ='No address'){}
+    constructor(public name:string, public address:string = 'No address'){}
 
 }
 
-const iroman = new Person('David','México');
+export class Hero extends Person{
+    constructor(public alterEgo:string, public age:number, public realName:string){
+        super(realName,'New York');
+    }
+}
+
+const iroman = new Hero('Iroman',45,'Tony');
 
 console.log(iroman);
